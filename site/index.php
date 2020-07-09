4990fb74006c036eb1e673bdd02db6a6712f50a3
aman14793 <aman14793@gmail.com>
2020-07-09 14:36:48 +0530

    php changes

---
+++ site/index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+echo "Hello World!";
+
+echo "<br><br>";
+
+
+print_r($_GET);
+
+echo $_GET["gender"];
+
+
+
+
+
+
+?>
+
+
+
+<p>What's your name</p>
+
+
+
+
+<form>
+
+    <input name="name" type="text">
+
+    <input type="submit" value="Go!">
+
+
+
+</form>
